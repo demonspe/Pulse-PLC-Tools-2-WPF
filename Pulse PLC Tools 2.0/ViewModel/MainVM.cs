4bf328348d60d515f81d3178597bc4df767b59b9
Владимир Султанов <Demonspe@gmail.com>
2018-09-13 17:00:58 +0500

    Собираю новый проект на MVVM

--- Pulse PLC Tools 2.0/ViewModel/MainVM.cs
+++ Pulse PLC Tools 2.0/ViewModel/MainVM.cs
@@ -7,10 +7,17 @@ using System.Threading.Tasks;
 
 namespace Pulse_PLC_Tools_2._0.ViewModel
 {
-    
-
     public class MainVM : BindableBase
     {
+        public ImpParams Imp1 { get; set; }
+        public ImpParams Imp2 { get; set; }
+        DeviceMainParams Device { get; set; }
 
+        public MainVM()
+        {
+            Imp1 = new ImpParams(1);
+            Imp2 = new ImpParams(2);
+            Device = new DeviceMainParams();
+        }
     }
 }
