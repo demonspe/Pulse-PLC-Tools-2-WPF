31c40a500cdd7a174867b1a5f9e9489ba49583a3
Владимир Султанов <Demonspe@gmail.com>
2018-09-13 15:47:27 +0500

    Добавил VM ImpParams

---
+++ Pulse PLC Tools 2.0/ViewModel/MainVM.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulse_PLC_Tools_2._0.ViewModel
+{
+    
+
+    public class MainVM : BindableBase
+    {
+
+    }
+}
