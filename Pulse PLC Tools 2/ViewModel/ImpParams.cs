e7514eaf76ddf6591864ff377a99237ff2b829e9
Владимир Султанов <Demonspe@gmail.com>
2018-09-15 16:58:27 +0500

    Описал страницы IMP сделал отдельные контролы для страниц tabpages

--- Pulse PLC Tools 2/ViewModel/ImpParams.cs
+++ Pulse PLC Tools 2/ViewModel/ImpParams.cs
@@ -7,50 +7,182 @@ using System.Threading.Tasks;
 
 namespace Pulse_PLC_Tools_2
 {
+    public enum ImpOverflowType: byte { Disable = 0, Overflow_5_Digits = 5, Overflow_6_Digits = 6 }
+    public enum ImpNumOfTarifs: byte { One = 1, Two = 2, Three = 3 }
+    public enum ImpAscueProtocolType : byte { PulsePLC = 0, Mercury230ART = 1 }
+
+    public class ImpTimeTarifs : BindableBase
+    {
+        public ImpTimeTarifs()
+        {
+            this.hours = 0;
+            this.minutes = 0;
+        }
+        public ImpTimeTarifs(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        private int hours;
+        private int minutes;
+
+        public int TimeInMinutes { get => hours * 60 + minutes; }
+        public int Hours { get => hours;
+            set
+            {
+                hours = value;
+                if (value > 23) hours = 23;
+                if (value < 0) hours = 0;
+                RaisePropertyChanged(nameof(Hours));
+                RaisePropertyChanged(nameof(TimeInMinutes));
+            }
+        }
+        public int Minutes { get => minutes;
+            set
+            {
+                minutes = value;
+                if (value > 59) minutes = 59;
+                if (value < 0) minutes = 0;
+                RaisePropertyChanged(nameof(Minutes));
+                RaisePropertyChanged(nameof(TimeInMinutes));
+            }
+        }
+    }
+
+    public class ImpEnergy : BindableBase
+    {
+        public ImpEnergy()
+        {
+            Value_Wt = 0;
+        }
+        public ImpEnergy(uint energyInWth)
+        {
+            Value_Wt = energyInWth;
+        }
+        private uint e; //Энергия в ваттах
+        private double e_kWt; //Энергия в киловатах
+
+        public uint Value_Wt
+        {
+            get => e;
+            set
+            {
+                e = value;
+                e_kWt = (double)e / 1000;
+                RaisePropertyChanged(nameof(Value_Wt));
+                RaisePropertyChanged(nameof(Value_kWt));
+            }
+        }
+        public double Value_kWt
+        {
+            get => e_kWt;
+            set
+            {
+                e_kWt = value;
+                e = Convert.ToUInt32(e_kWt * 1000);
+                RaisePropertyChanged(nameof(Value_Wt));
+                RaisePropertyChanged(nameof(Value_kWt));
+            }
+        }
+    }
+
     public class ImpParams : BindableBase
     {
         private  byte isEnable;      //Включен/отключен
         private  byte adrs_PLC;       //Сетевой адрес
-        private  UInt16 a;            //A передаточное число
-        private  UInt32 e_T1;         //Энергия ПО ТАРИФУ 1
-        private  UInt32 e_T2;         //Энергия ПО ТАРИФУ 2
-        private  UInt32 e_T3;         //Энергия ПО ТАРИФУ 3
-        private  UInt32 e_T1_Start;   //Энергия На начало суток ПО ТАРИФУ 1
-        private  UInt32 e_T2_Start;   //Энергия На начало суток ПО ТАРИФУ 2
-        private  UInt32 e_T3_Start;   //Энергия На начало суток ПО ТАРИФУ 3
-        private  byte perepoln;       //число разрядов после которых происходит переполнение 0, 5 или 6
-        private  byte t_qty;         //Количество тарифов
-        private  UInt16 t1_Time_1;   //Начало первой пиковой зоны в минутах
-        private  UInt16 t3_Time_1;   //Начало первой полупиковой зоны в минутах
-        private  UInt16 t1_Time_2;   //Начало второй пиковой зоны в минутах
-        private  UInt16 t3_Time_2;   //Начало второй полупиковой зоны в минутах
-        private  UInt16 t2_Time;     //Начало ночной зоны в минутах
-        private  UInt16 ascue_adrs;  //Адрес для протокола
-        private  byte[] ascue_pass;  //Пароль для протокола
-        private  byte ascue_protocol;    //Тип протокола
-        private  UInt16 max_Power;    //Максимальная мощность нагрузки
-
+        private ushort a;            //A передаточное число
+        private ImpOverflowType perepoln;       //число разрядов после которых происходит переполнение 0, 5 или 6
+        private int perepoln_view;  //число разрядов после которых происходит переполнение 0, 1 или 2 для отображения во view (ComboBox)
+        private ImpNumOfTarifs t_qty;         //Количество тарифов
+        private int t_qty_view;    //Количество тарифов для отображения во view (ComboBox)
+        private ushort ascue_adrs;  //Адрес для протокола
+        private byte[] ascue_pass;  //Пароль для протокола
+        private string ascue_pass_string;  //Пароль для протокола
+        private byte ascue_protocol;    //Тип протокола
+        private ushort max_Power;    //Максимальная мощность нагрузки
 
         public byte IsEnable { get => isEnable; set { isEnable = value; RaisePropertyChanged(nameof(IsEnable)); } }
         public byte Adrs_PLC { get => adrs_PLC; set { adrs_PLC = value; RaisePropertyChanged(nameof(Adrs_PLC)); } }
         public ushort A { get => a; set { a = value; RaisePropertyChanged(nameof(A)); } }
-        public uint E_T1 { get => e_T1; set { e_T1 = value; RaisePropertyChanged(nameof(E_T1)); RaisePropertyChanged(nameof(E_Tsum)); } }
-        public uint E_T2 { get => e_T2; set { e_T2 = value; RaisePropertyChanged(nameof(E_T2)); RaisePropertyChanged(nameof(E_Tsum)); } }
-        public uint E_T3 { get => e_T3; set { e_T3 = value; RaisePropertyChanged(nameof(E_T3)); RaisePropertyChanged(nameof(E_Tsum)); } }
-        public uint E_Tsum { get => E_T1 + E_T2 + E_T3; }
-        public uint E_T1_Start { get => e_T1_Start; set { e_T1_Start = value; RaisePropertyChanged(nameof(E_T1_Start)); RaisePropertyChanged(nameof(E_Tsum_Start)); } }
-        public uint E_T2_Start { get => e_T2_Start; set { e_T2_Start = value; RaisePropertyChanged(nameof(E_T2_Start)); RaisePropertyChanged(nameof(E_Tsum_Start)); } }
-        public uint E_T3_Start { get => e_T3_Start; set { e_T3_Start = value; RaisePropertyChanged(nameof(E_T3_Start)); RaisePropertyChanged(nameof(E_Tsum_Start)); } }
-        public uint E_Tsum_Start { get => E_T1_Start + E_T2_Start + E_T3_Start; }
-        public byte Perepoln { get => perepoln; set { perepoln = value; RaisePropertyChanged(nameof(Perepoln)); } }
-        public byte T_qty { get => t_qty; set { t_qty = value; RaisePropertyChanged(nameof(T_qty)); } }
-        public ushort T1_Time_1 { get => t1_Time_1; set { t1_Time_1 = value; RaisePropertyChanged(nameof(T1_Time_1)); } }
-        public ushort T3_Time_1 { get => t3_Time_1; set { t3_Time_1 = value; RaisePropertyChanged(nameof(T3_Time_1)); } }
-        public ushort T1_Time_2 { get => t1_Time_2; set { t1_Time_2 = value; RaisePropertyChanged(nameof(T1_Time_2)); } }
-        public ushort T3_Time_2 { get => t3_Time_2; set { t3_Time_2 = value; RaisePropertyChanged(nameof(T3_Time_2)); } }
-        public ushort T2_Time { get => t2_Time; set { t2_Time = value; RaisePropertyChanged(nameof(T2_Time)); } }
+        public ImpEnergy E_T1 { get; set; }
+        public ImpEnergy E_T2 { get; set; }
+        public ImpEnergy E_T3 { get; set; }
+        public double E_Tsum_kWt { get => E_T1.Value_kWt + E_T2.Value_kWt + E_T3.Value_kWt; }
+        public ImpEnergy E_T1_Start { get; set; }
+        public ImpEnergy E_T2_Start { get; set; }
+        public ImpEnergy E_T3_Start { get; set; }
+        public double E_Tsum_Start_kWt { get => E_T1_Start.Value_kWt + E_T2_Start.Value_kWt + E_T3_Start.Value_kWt; }
+        public ImpOverflowType Perepoln { get => perepoln;
+            set {
+                perepoln = value;
+                if (perepoln == ImpOverflowType.Disable) perepoln_view = 0;
+                if (perepoln == ImpOverflowType.Overflow_5_Digits) perepoln_view = 1;
+                if (perepoln == ImpOverflowType.Overflow_6_Digits) perepoln_view = 2;
+                RaisePropertyChanged(nameof(Perepoln));
+                RaisePropertyChanged(nameof(Perepoln_View));
+            }
+        }
+        public int Perepoln_View { get => perepoln_view;
+            set {
+                perepoln_view = value;
+                if (perepoln_view == 0) perepoln = ImpOverflowType.Disable;
+                if (perepoln_view == 1) perepoln = ImpOverflowType.Overflow_5_Digits;
+                if (perepoln_view == 2) perepoln = ImpOverflowType.Overflow_6_Digits;
+                RaisePropertyChanged(nameof(Perepoln));
+                RaisePropertyChanged(nameof(Perepoln_View));
+            }
+        }
+        public ImpNumOfTarifs T_qty { get => t_qty;
+            set {
+                t_qty = value;
+                if (t_qty == ImpNumOfTarifs.One) t_qty_view = 0;
+                if (t_qty == ImpNumOfTarifs.Two) t_qty_view = 1;
+                if (t_qty == ImpNumOfTarifs.Three) t_qty_view = 2;
+                RaisePropertyChanged(nameof(T_qty));
+                RaisePropertyChanged(nameof(T_qty_View));
+            }
+        }
+        public int T_qty_View { get => t_qty_view;
+            set {
+                t_qty_view = value;
+                if (t_qty_view == 0) t_qty = ImpNumOfTarifs.One;
+                if (t_qty_view == 1) t_qty = ImpNumOfTarifs.Two;
+                if (t_qty_view == 2) t_qty = ImpNumOfTarifs.Three;
+                RaisePropertyChanged(nameof(T_qty));
+                RaisePropertyChanged(nameof(T_qty_View));
+            }
+        }
+        public ImpTimeTarifs T1_Time_1 { get; set; }
+        public ImpTimeTarifs T3_Time_1 { get; set; }
+        public ImpTimeTarifs T1_Time_2 { get; set; }
+        public ImpTimeTarifs T3_Time_2 { get; set; }
+        public ImpTimeTarifs T2_Time { get; set; }
         public ushort Ascue_adrs { get => ascue_adrs; set { ascue_adrs = value; RaisePropertyChanged(nameof(Ascue_adrs)); } }
-        public byte[] Ascue_pass { get => ascue_pass; set { ascue_pass = value; RaisePropertyChanged(nameof(Ascue_pass)); } }
+        public byte[] Ascue_pass { get => ascue_pass;
+            set {
+                ascue_pass = value;
+                ascue_pass_string = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    ascue_pass_string += (i < ascue_pass.Length) ? ascue_pass[i].ToString() : "0";
+                }
+                RaisePropertyChanged(nameof(Ascue_pass));
+                RaisePropertyChanged(nameof(Ascue_pass_View));
+            }
+        }
+        public string Ascue_pass_View { get => ascue_pass_string;
+            set {
+                ascue_pass_string = value;
+                ascue_pass = new byte[6] { 0, 0, 0, 0, 0, 0 };
+                for (int i = 0; i < 6; i++)
+                {
+                    if (char.IsDigit(ascue_pass_string[i])) ascue_pass[i] = Convert.ToByte(ascue_pass_string[i]);
+                }
+                RaisePropertyChanged(nameof(Ascue_pass));
+                RaisePropertyChanged(nameof(Ascue_pass_View));
+            }
+        }
         public byte Ascue_protocol { get => ascue_protocol; set { ascue_protocol = value; RaisePropertyChanged(nameof(Ascue_protocol)); } }
         public ushort Max_Power { get => max_Power; set { max_Power = value; RaisePropertyChanged(nameof(Max_Power)); } }
 
@@ -64,23 +196,34 @@ namespace Pulse_PLC_Tools_2
         {
             IsEnable = 0;
             A = 1600;
-            E_T1 = 0;
-            E_T2 = 0;
-            E_T3 = 0;
-            E_T1_Start = 0;
-            E_T2_Start = 0;
-            E_T3_Start = 0;
-            Perepoln = 0;
-            T_qty = 1;
-            T1_Time_1 = 7 * 60;
-            T3_Time_1 = 10 * 60;
-            T1_Time_2 = 17 * 60;
-            T3_Time_2 = 21 * 60;
-            T2_Time = 23 * 60;
+
+            E_T1 = new ImpEnergy();
+            E_T2 = new ImpEnergy();
+            E_T3 = new ImpEnergy();
+            E_T1.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_kWt)); };
+            E_T2.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_kWt)); };
+            E_T3.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_kWt)); };
+
+            E_T1_Start = new ImpEnergy();
+            E_T2_Start = new ImpEnergy();
+            E_T3_Start = new ImpEnergy();
+            E_T1_Start.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_Start_kWt)); };
+            E_T2_Start.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_Start_kWt)); };
+            E_T3_Start.PropertyChanged += (s, a) => { RaisePropertyChanged(nameof(E_Tsum_Start_kWt)); };
+
+            Perepoln = ImpOverflowType.Disable;
+            T_qty = ImpNumOfTarifs.One;
+            T1_Time_1 = new ImpTimeTarifs(7, 0); 
+            T3_Time_1 = new ImpTimeTarifs(10, 0);
+            T1_Time_2 = new ImpTimeTarifs(17, 0);
+            T3_Time_2 = new ImpTimeTarifs(21, 0);
+            T2_Time = new ImpTimeTarifs(23, 0);
             Ascue_adrs = 0;
             Ascue_pass = new byte[6] { 1, 1, 1, 1, 1, 1 };
-            Ascue_protocol = 0;
+            Ascue_protocol = (byte)ImpAscueProtocolType.PulsePLC;
             Max_Power = 0;
         }
+
+
     }
 }
